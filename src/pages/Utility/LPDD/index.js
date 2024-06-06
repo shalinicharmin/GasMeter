f2d1a432498c44dca63f1cb2938548d2c2ccb621
gp-shalini <you@example.com>
2024-06-06 12:05:21 +0530

    Tabs added in Lpdd

--- src/pages/Utility/LPDD/index.js
+++ src/pages/Utility/LPDD/index.js
@@ -1,7 +1,43 @@
-import React from "react"
+// import React from "react"
+
+// const LPDDUtility = () => {
+//   return <div>LPDDUtility</div>
+// }
+
+// export default LPDDUtility
+
+import React, { useState } from "react"
+import { Tabs } from "antd"
+// import CommandHistory from "./CommandHistoryWrapper/commandHistory"
+// import PushData from "./PushDataWrapper/pushData"
+// import MeterProfile from "./MeterProfilewrapper/meterProfile"
+import "../../../styles/tabs.scss"
+
+const { TabPane } = Tabs
 
 const LPDDUtility = () => {
-  return <div>LPDDUtility</div>
+  const [activeKey, setActiveKey] = useState("1")
+
+  const handleChange = (key) => {
+    setActiveKey(key)
+  }
+
+  return (
+    <Tabs activeKey={activeKey} onChange={handleChange} type='card'>
+      <TabPane tab='Command History' key='1'>
+        <p>hello 1</p>
+        {/* {activeKey === "1" && <CommandHistory />} */}
+      </TabPane>
+      <TabPane tab='Push Data' key='2'>
+        <p>hello 2</p>
+        {/* {activeKey === "2" && <PushData />} */}
+      </TabPane>
+      <TabPane tab='Meter Profile' key='3'>
+        <p>hello 3</p>
+        {/* {activeKey === "3" && <MeterProfile />} */}
+      </TabPane>
+    </Tabs>
+  )
 }
 
 export default LPDDUtility
